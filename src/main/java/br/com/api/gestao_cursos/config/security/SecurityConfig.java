62e7d3e762f2427049b20f936bb242612fd4d032
Hudson <hudsonsistemasp@gmail.com>
2025-07-29 10:54:54 -0300

    Criação da classe SecurityConfig para configuração para iniciar algumas politicas de seguranças e geração da criptografia do password, Troca da chave primária na Camada Repository de Long para UUID, incremento na classe RoleUser para que a descrição seja persistida no banco de dados e não o index.

--- src/main/java/br/com/api/gestao_cursos/config/security/SecurityConfig.java
+++ src/main/java/br/com/api/gestao_cursos/config/security/SecurityConfig.java
@@ -1,4 +1,33 @@
 package br.com.api.gestao_cursos.config.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    //Método do Spring Security para personalizarmos algumas políticas de segurança
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //csrf é uma das políticas de segurança. Aqui eu desabilito o método de autenticação geral das páginas, pois vou personalizar as que quero
+        http.csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // exibe H2 Console via iframe
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll() // libera acesso ao H2 Console
+                        .anyRequest().permitAll() // todas as requisições são liberadas temporariamente
+                );
+
+        return http.build();
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }

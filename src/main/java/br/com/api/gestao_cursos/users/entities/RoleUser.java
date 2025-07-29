d6759ea6a0b5870fe537368c0692af999c2c50ec
Hudson <hudsonsistemasp@gmail.com>
2025-07-29 11:02:27 -0300

    incremento na classe RoleUser para que a descrição seja persistida no banco de dados e não o index.

--- src/main/java/br/com/api/gestao_cursos/users/entities/RoleUser.java
+++ src/main/java/br/com/api/gestao_cursos/users/entities/RoleUser.java
@@ -1,5 +1,14 @@
 package br.com.api.gestao_cursos.users.entities;
 
 public enum RoleUser {
-    PROFESSOR, ALUNO
+    PROFESSOR("PROFESSOR"), ALUNO("ALUNO");
+
+    private String description;
+
+    RoleUser(String description){
+        this.description = description;
+    }
+     public String getDescription(){
+        return this.description;
+     }
 }

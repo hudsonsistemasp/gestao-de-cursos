adcc0b4e32c90dc74af4a0127a7bd8596e6c7890
Hudson <hudsonsistemasp@gmail.com>
2025-07-29 11:00:54 -0300

    Troca da chave primária na Camada Repository de Long para UUID, incremento na classe RoleUser para que a descrição seja persistida no banco de dados e não o index.

--- src/main/java/br/com/api/gestao_cursos/repository/UserRepository.java
+++ src/main/java/br/com/api/gestao_cursos/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmail(String email);
+
 }

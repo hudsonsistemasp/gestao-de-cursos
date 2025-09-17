462cfcf68604c6f7f4b947541311996923cfaefe
Hudson <hudsonsistemasp@gmail.com>
2025-09-17 20:53:16 -0300

    Inclusão das anotações @AllArgsConstructor e @NoArgsConstructor para elaboração do teste unitário.

--- src/main/java/br/com/api/gestao_cursos/modulesComponents/users/dto/CreateUserRequestDto.java
+++ src/main/java/br/com/api/gestao_cursos/modulesComponents/users/dto/CreateUserRequestDto.java
@@ -5,9 +5,15 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateUserRequestDto {
 
     @NotBlank(message = "Nome do usuário é obrigatório")

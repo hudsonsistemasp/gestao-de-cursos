74997c53a045f04be5a77dfa4b6e1d4f6c65a77a
Hudson <hudsonsistemasp@gmail.com>
2025-10-01 20:06:18 -0300

    inclusão das anotações @Builder, @AllArgsConstructor e @NoArgsConstructor para construção dos testes unitários.

--- src/main/java/br/com/api/gestao_cursos/modulesComponents/auth/dto/CreateAuthRequestDto.java
+++ src/main/java/br/com/api/gestao_cursos/modulesComponents/auth/dto/CreateAuthRequestDto.java
@@ -1,8 +1,14 @@
 package br.com.api.gestao_cursos.modulesComponents.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateAuthRequestDto {
     private String email;
     private String password;

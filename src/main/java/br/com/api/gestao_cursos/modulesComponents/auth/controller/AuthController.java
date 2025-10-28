7d4ffd40f375c6b33c88ca941d3a1803379e18c2
Hudson <hudsonsistemasp@gmail.com>
2025-10-28 11:54:17 -0300

    Criação do catch UserNameNotFoundException na camada AuthController, devido ao novo requisito de validação do email do user request e do que existe no banco de dados.

--- src/main/java/br/com/api/gestao_cursos/modulesComponents/auth/controller/AuthController.java
+++ src/main/java/br/com/api/gestao_cursos/modulesComponents/auth/controller/AuthController.java
@@ -6,10 +6,13 @@ import br.com.api.gestao_cursos.modulesComponents.users.entities.RoleUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 
 @RestController
 @RequestMapping("/auth")
@@ -32,9 +35,11 @@ public class AuthController {
 
     @PostMapping("/student")
     public ResponseEntity<?> authStudent(@RequestBody CreateAuthRequestDto createAuthRequestDto){
-        try{
+        try {
             var token = createAuthUseCase.create(createAuthRequestDto, RoleUser.student);
             return ResponseEntity.ok(token);
+        } catch (UsernameNotFoundException ex) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }

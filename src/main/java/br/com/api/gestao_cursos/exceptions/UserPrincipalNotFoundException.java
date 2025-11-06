16aed1881434353418965bb0d5c02b03660e177f
Hudson <hudsonsistemasp@gmail.com>
2025-11-06 12:05:03 -0300

    Criação de nova classe exception UserPrincipalNotFoundException para aplicação do usuário inexistente no banco e com isso teve fácil aplicação no teste de integração.

---
+++ src/main/java/br/com/api/gestao_cursos/exceptions/UserPrincipalNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.api.gestao_cursos.exceptions;
+
+public class UserPrincipalNotFoundException extends Exception {
+    public UserPrincipalNotFoundException(String message) {
+        super(message);
+    }
+}
